90e744cafaa1ecff86371e015cc8d6b3a1231a36
Lubricant Jam <LubricantJam@users.noreply.github.com>
2020-07-19 17:37:00 +0100

    Passengers can now see speedometers.

--- [Main]/speedometer/client.lua
+++ [Main]/speedometer/client.lua
@@ -5,7 +5,7 @@ Citizen.CreateThread(function()
 		local Ped = GetPlayerPed(-1)
 		if(IsPedInAnyVehicle(Ped)) then
 			local PedCar = GetVehiclePedIsIn(GetPlayerPed(-1))
-			if PedCar and GetPedInVehicleSeat(PedCar, -1) == Ped then
+			if PedCar then
 
 				-- Speed
 				carSpeed = math.ceil(GetEntitySpeed(PedCar) * 2.236936)
