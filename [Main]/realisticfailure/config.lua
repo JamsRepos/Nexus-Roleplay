1dbbd0d99a64ac327d9c819ea22b0cef8ebf5d48
Lubricant Jam <LubricantJam@users.noreply.github.com>
2020-07-29 14:16:56 +0100

    Increased vehicle damage & random tire pop chance.

--- [Main]/realisticfailure/config.lua
+++ [Main]/realisticfailure/config.lua
@@ -4,16 +4,16 @@ Config = {
 	deformationExponent = 0.4,					-- How much should the handling file deformation setting be compressed toward 0.35. (Make cars more similar). A value of 1=no change. Lower values will compress more, values above 1 it will expand. Dont set to zero or negative.
 	collisionDamageExponent = 0.6,				-- How much should the handling file deformation setting be compressed toward 0.35. (Make cars more similar). A value of 1=no change. Lower values will compress more, values above 1 it will expand. Dont set to zero or negative.
 
-	damageFactorEngine = 5.0,					-- Sane values are 1 to 100. Higher values means more damage to vehicle. A good starting point is 10
-	damageFactorBody = 5.0,						-- Sane values are 1 to 100. Higher values means more damage to vehicle. A good starting point is 10
+	damageFactorEngine = 10.0,					-- Sane values are 1 to 100. Higher values means more damage to vehicle. A good starting point is 10
+	damageFactorBody = 10.0,					-- Sane values are 1 to 100. Higher values means more damage to vehicle. A good starting point is 10
 	damageFactorPetrolTank = 64.0,				-- Sane values are 1 to 200. Higher values means more damage to vehicle. A good starting point is 64
 	engineDamageExponent = 0.6,					-- How much should the handling file engine damage setting be compressed toward 0.35. (Make cars more similar). A value of 1=no change. Lower values will compress more, values above 1 it will expand. Dont set to zero or negative.
 	weaponsDamageMultiplier = 0.01,				-- How much damage should the vehicle get from weapons fire. Range 0.0 to 10.0, where 0.0 is no damage and 10.0 is 10x damage. -1 = don't touch
-	degradingHealthSpeedFactor = 2,			-- Speed of slowly degrading health, but not failure. Value of 10 means that it will take about 0.25 second per health point, so degradation from 800 to 305 will take about 2 minutes of clean driving. Higher values means faster degradation
-	cascadingFailureSpeedFactor = 3.0,			-- Sane values are 1 to 100. When vehicle health drops below a certain point, cascading failure sets in, and the health drops rapidly until the vehicle dies. Higher values means faster failure. A good starting point is 8
+	degradingHealthSpeedFactor = 10,			-- Speed of slowly degrading health, but not failure. Value of 10 means that it will take about 0.25 second per health point, so degradation from 800 to 305 will take about 2 minutes of clean driving. Higher values means faster degradation
+	cascadingFailureSpeedFactor = 8.0,			-- Sane values are 1 to 100. When vehicle health drops below a certain point, cascading failure sets in, and the health drops rapidly until the vehicle dies. Higher values means faster failure. A good starting point is 8
 
-	degradingFailureThreshold = 300.0,			-- Below this value, slow health degradation will set in
-	cascadingFailureThreshold = 250.0,			-- Below this value, health cascading failure will set in
+	degradingFailureThreshold = 800.0,			-- Below this value, slow health degradation will set in
+	cascadingFailureThreshold = 360.0,			-- Below this value, health cascading failure will set in
 	engineSafeGuard = 100.0,					-- Final failure value. Set it too high, and the vehicle won't smoke when disabled. Set too low, and the car will catch fire from a single bullet to the engine. At health 100 a typical car can take 3-4 bullets to the engine before catching fire.
 
 	torqueMultiplierEnabled = true,				-- Decrease engine torque as engine gets more and more damaged
@@ -29,33 +29,33 @@ Config = {
 
 	compatibilityMode = false,					-- prevents other scripts from modifying the fuel tank health to avoid random engine failure with BVA 0.351 (Downside is it disabled explosion prevention)
 
-	randomTireBurstInterval = 0,				-- Number of minutes (statistically, not precisely) to drive above 22 mph before you get a tire puncture. 0=feature is disabled
+	randomTireBurstInterval = 120,				-- Number of minutes (statistically, not precisely) to drive above 22 mph before you get a tire puncture. 0=feature is disabled
 
 	-- Class Damagefactor Multiplier
 	-- The damageFactor for engine, body and Petroltank will be multiplied by this value, depending on vehicle class
 	-- Use it to increase or decrease damage for each class
 	classDamageMultiplier = {
-		[0] = 	0.35,		--	0: Compacts
-				0.35,		--	1: Sedans
-				0.30,		--	2: SUVs
-				0.35,		--	3: Coupes
-				0.35,		--	4: Muscle
-				0.35,		--	5: Sports Classics
-				0.35,		--	6: Sports
-				0.35,		--	7: Super
-				0.15,		--	8: Motorcycles
-				0.45,		--	9: Off-road
-				0.15,		--	10: Industrial
-				0.35,		--	11: Utility
-				0.35,		--	12: Vans
-				0.35,		--	13: Cycles
+		[0] = 	1.0,		--	0: Compacts
+				1.0,		--	1: Sedans
+				1.0,		--	2: SUVs
+				1.0,		--	3: Coupes
+				1.0,		--	4: Muscle
+				1.0,		--	5: Sports Classics
+				1.0,		--	6: Sports
+				1.0,		--	7: Super
+				0.25,		--	8: Motorcycles
+				0.7,		--	9: Off-road
+				0.25,		--	10: Industrial
+				1.0,		--	11: Utility
+				1.0,		--	12: Vans
+				1.0,		--	13: Cycles
 				0.5,		--	14: Boats
-				0.35,		--	15: Helicopters
-				0.35,		--	16: Planes
-				0.35,		--	17: Service
-				0.25,		--	18: Emergency
-				0.15,		--	19: Military
-				0.35,		--	20: Commercial
-				0.35			--	21: Trains
+				1.0,		--	15: Helicopters
+				1.0,		--	16: Planes
+				1.0,		--	17: Service
+				0.75,		--	18: Emergency
+				0.75,		--	19: Military
+				1.0,		--	20: Commercial
+				1.0			--	21: Trains
 	}
 }
