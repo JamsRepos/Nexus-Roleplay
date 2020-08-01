c6924c121dca20ee8b6a53d10f38b8a5c7c9250a
Lubricant Jam <LubricantJam@users.noreply.github.com>
2020-08-01 21:04:29 +0100

    Fixed vehicles not saving all mods at LSC.

--- [Main]/NRP-Tuners/lscustoms.lua
+++ [Main]/NRP-Tuners/lscustoms.lua
@@ -565,9 +565,9 @@ local function DriveOutOfGarage(pos)
 		pos = currentpos
 		TaskVehicleDriveToCoord(ped, veh, pos.outside.x, pos.outside.y, pos.outside.z, f(5), f(0.1), GetEntityModel(veh), 16777216, f(0.1), true)
 		pos = currentpos.driveout
-		plate = GetVehicleNumberPlateText(veh)
+		plate = exports['core']:TrimPlate(GetVehicleNumberPlateText(veh))
 		vehProps = GetVehProps(veh)
-		TriggerServerEvent("LSC:finished", plate, vehProps)
+		TriggerServerEvent("LSC:finished2", plate, vehProps)
 		
 		StartFade()
 		Citizen.Wait(500)
