48d948394e23758e8009fe017e697e4bdf9d5f7e
Raff <raffsimms23@gmail.com>
2020-08-04 15:57:15 +0100

    Фввув фззднштп фкьщк дщфвштп ифк

--- [Main]/core/inventory/items.lua
+++ [Main]/core/inventory/items.lua
@@ -716,8 +716,10 @@ end)
 
 RegisterNetEvent('items:bulletproof_vest')
 AddEventHandler('items:bulletproof_vest', function()
-	AddArmourToPed(GetPlayerPed(-1), 100)
-	SetPedArmour(GetPlayerPed(-1), 100)
+	exports['pogressBar']:drawBar(2000, 'Applying Armor', function()
+		AddArmourToPed(GetPlayerPed(-1), 100)
+		SetPedArmour(GetPlayerPed(-1), 100)
+	end)
 end)
 
 Citizen.CreateThread(function()
