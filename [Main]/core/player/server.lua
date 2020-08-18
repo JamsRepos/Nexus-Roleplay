fa9b0b6af698b9d56110bca21e9e2a8b07e2efac
Lubricant Jam <LubricantJam@users.noreply.github.com>
2020-08-19 00:04:15 +0100

    Added VPN blocker.

--- [Main]/core/player/server.lua
+++ [Main]/core/player/server.lua
@@ -5,6 +5,39 @@ groups = {}
 Group = {}
 Group.__index = Group
 
+AddEventHandler("playerConnecting", function(name, setKickReason, deferrals)
+    local player = source
+    local name, setKickReason, deferrals = name, setKickReason, deferrals;
+    local ipIdentifier
+    local identifiers = GetPlayerIdentifiers(player)
+    deferrals.defer()
+    Wait(0)
+    deferrals.update(string.format("Hello %s. Your IP Address is being checked.", name))
+    for _, v in pairs(identifiers) do
+        if string.find(v, "ip") then
+            ipIdentifier = v:sub(4)
+            break
+        end
+    end
+    Wait(0)
+    if not ipIdentifier then
+        deferrals.done("We could not find your IP Address.")
+    else
+        PerformHttpRequest("http://proxycheck.io/v2/" .. ipIdentifier .. "?key=06rbj9-65d161-484056-y08c3d&vpn=1&days=2", function(err, text, headers)
+            if tonumber(err) == 200 then
+                local tbl = json.decode(text)
+                if tbl["proxy"] == false then
+                    deferrals.done()
+                else
+                    deferrals.done("You are using a VPN. Please disable and try again.")
+                end
+            else
+                deferrals.done("There was an error in the API. Please go to our Discord for support.")
+            end
+        end)
+    end
+end)
+
 RegisterServerEvent('core:checkuser')
 AddEventHandler('core:checkuser', function()
  local source = tonumber(source)
