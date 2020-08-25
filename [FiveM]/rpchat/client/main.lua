01941e18b1ba416ac8729ef96871e72705aed71e
Raff <raffsimms23@gmail.com>
2020-08-25 16:26:19 +0100

    Added toggle Local OOC command

--- [FiveM]/rpchat/client/main.lua
+++ [FiveM]/rpchat/client/main.lua
@@ -4,13 +4,15 @@
 
 --]]
 
+local ooc = true 
+
 RegisterNetEvent('sendProximityMessage')
 AddEventHandler('sendProximityMessage', function(id, name, message)
   local myId = PlayerId()
   local pid = GetPlayerFromServerId(id)
-  if pid == myId then
+  if pid == myId and ooc then
     TriggerEvent('chatMessage', "^7[^2Local OOC^7] ^2" .. name .. ": ", {0, 153, 204}, "^7 " .. message)
-  elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(myId)), GetEntityCoords(GetPlayerPed(pid)), true) < 19.999 then
+  elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(myId)), GetEntityCoords(GetPlayerPed(pid)), true) < 19.999 and ooc then
     TriggerEvent('chatMessage', "^7[^2Local OOC^7] ^2" .. name .. ": ", {0, 153, 204}, "^7 " .. message)
   end
 end)
@@ -34,5 +36,15 @@ AddEventHandler('sendProximityMessageDice', function(id, name, message)
     TriggerEvent('chatMessage', "^7[^5Dice^7] ^5" .. name .. " ", {0, 153, 204}, "^5 " .. message)
   elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(myId)), GetEntityCoords(GetPlayerPed(pid)), true) < 9.999 then
     TriggerEvent('chatMessage', "^7[^5Dice^7] ^5" .. name .. " ", {0, 153, 204}, "^5 " .. message)
+  end
+end)
+
+RegisterCommand('toggleooc', function()
+  if ooc then 
+    ooc = false
+    exports['NRP-notify']:DoHudText('error', 'Local OOC Toggled Off')
+  else
+    ooc = true
+    exports['NRP-notify']:DoHudText('success', 'Local OOC Toggled On')
   end
 end)
